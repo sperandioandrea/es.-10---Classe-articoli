7ad0b736161d6d6f293d63e72d3bb63766a44c33
Sperandio.20200 <Sperandio.20200@E2-9-PC12>
2023-11-21 13:24:26 +0100

    Metodo Equals

--- es. 10 - Classe articoli/Articolo.cs
+++ es. 10 - Classe articoli/Articolo.cs
@@ -6,7 +6,55 @@ using System.Threading.Tasks;
 
 namespace es._10___Classe_articoli
 {
-    internal class Articolo
+    class Articolo
     {
+        //Attributi
+        protected int _codice;
+        protected string _descrizione;
+        protected int _prezzo;
+        protected bool _fedeltà;
+
+        //Costruttore
+        public Articolo(int Codice, string Descrizione, int Prezzo)
+        {
+            _codice = Codice;
+            _descrizione = Descrizione;
+            _prezzo = Prezzo;
+        }
+
+        //metodi GET & SET
+        public int Codice
+        {
+            get { return _codice; }
+            set { _codice = value; }
+        }
+        public string Descrizione
+        {
+            get { return _descrizione; }
+            set { _descrizione = value; }
+        }
+        public int Prezzo
+        {
+            get { return _prezzo; }
+            set { _prezzo = value; }
+        }
+
+        //Metodo Equals
+        public override bool Equals(Object obj)
+        {
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            return true;
+            Articolo metodo = (Articolo)obj;
+            if (_codice == metodo._codice && _descrizione == metodo._descrizione && _prezzo == metodo._prezzo && _fedeltà == metodo._fedeltà)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
     }
 }
--- es. 10 - Classe articoli/ArticoloAlimentare.cs
+++ es. 10 - Classe articoli/ArticoloAlimentare.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Security;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace es._10___Classe_articoli
 {
-    internal class ArticoloAlimentare
+    class ArticoloAlimentare
     {
+
     }
+    
 }
