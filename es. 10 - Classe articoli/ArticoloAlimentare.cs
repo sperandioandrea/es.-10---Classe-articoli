2316f6dab1d0f4e29177a8c0d97006941a76fbff
Sperandio.20200 <Sperandio.20200@E2-9-PC12>
2023-11-21 13:25:24 +0100

    Attributi classe ArticoloAlimentare e richiamo attributi

--- es. 10 - Classe articoli/ArticoloAlimentare.cs
+++ es. 10 - Classe articoli/ArticoloAlimentare.cs
@@ -7,9 +7,21 @@ using System.Threading.Tasks;
 
 namespace es._10___Classe_articoli
 {
-    class ArticoloAlimentare
+    class ArticoloAlimentare : Articolo
     {
+        private int anno;
 
+        public int Anno
+        {
+            set { anno = value; }
+            get { return anno; }
+        }
+
+        public ArticoloAlimentare(int Codice, string Descrizione, int Prezzo) : base(Codice, Descrizione, Prezzo)
+        {
+
+        }
+    
     }
     
 }
